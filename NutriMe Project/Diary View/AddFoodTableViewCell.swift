432b3da9400a3fbdd3836e1ee2d33bf448db6bb6
Leonnardo Benjamin Hutama <leonnardo.hutama@binusa.c.id>
2020-01-08 21:17:06 +0700

    Rabu Final

--- NutriMe Project/Diary View/AddFoodTableViewCell.swift
+++ NutriMe Project/Diary View/AddFoodTableViewCell.swift
@@ -9,32 +9,34 @@
 import UIKit
 
 protocol ButtonAddFood{
-  func buttonClicked(section: EatCategory)
-  func sendFoodData(food: UserFood)
+    func buttonClicked(section: EatCategory)
+    func sendFoodData(food: UserFood)
 }
 
 extension ButtonAddFood{
-  func sendFoodData(food: Food){}
+    func sendFoodData(food: Food){
+        
+    }
 }
 
 class AddFoodTableViewCell: UITableViewCell {
-
-  var delegate: ButtonAddFood?
-  var section: EatCategory!
-  
-  @IBAction func btnAddFood(_ sender: Any) {
-    delegate?.buttonClicked(section: section)
-  }
-  
-  override func awakeFromNib() {
+    
+    var delegate: ButtonAddFood?
+    var section: EatCategory!
+    
+    @IBAction func btnAddFood(_ sender: Any) {
+        delegate?.buttonClicked(section: section)
+    }
+    
+    override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
